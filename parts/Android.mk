ca79b3f865750d9e7d36a91015779b63bab80a1d
TheScarastic <warabhishek@gmail.com>
2019-06-14 14:24:30 +0530

    davinci: parts: Initial popup camera impl
    Change-Id: Ib3f1840401c5fb6b77656da4ee0f8e5f387aaaec

--- parts/Android.mk
+++ parts/Android.mk
@@ -24,12 +24,13 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    vendor.xiaomi.hardware.displayfeature-V1.0-java \
     vendor.xiaomi.hardware.motor-V1.0-java
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
     $(TOP)/packages/resources/devicesettings/res
+
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include frameworks/base/packages/SettingsLib/common.mk
 
